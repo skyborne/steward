7e558f2b87af254256fa5c95f369bbdfcbedc83a
devericx <devericx@gmail.com>
2017-07-21 16:45:01 -0400

    get message from latest email

--- server.py
+++ server.py
@@ -1,8 +1,39 @@
 #!/usr/bin/env python3
 
-# Check for email and download the email
-
 from uuid import uuid4
+
+import imaplib
+import email
 
 def generate_key():
     return uuid4()
+
+def fetch_mail(uuid):
+    mail = imaplib.IMAP4_SSL("imap.gmail.com")
+    mail.login("skyborne.info@gmail.com", open('.keys/GMAIL', 'r').readline().rstrip())
+    mail.list()
+    mail.select("inbox")
+
+    result, data = mail.search(None, "ALL")
+
+    ids = data[0]
+    id_list = ids.split()
+    latest_email_id = id_list[-1]
+
+    result, data = mail.fetch(latest_email_id, "(RFC822)")
+
+    raw_email = data[0][1]
+    raw_email_string = raw_email.decode("utf-8")
+
+    message = email.message_from_string(raw_email_string)
+
+    return message
+
+def parse_mail(email):
+    pass
+
+def main():
+    print(fetch_mail(1))
+
+if __name__ == "__main__":
+    main()
---
+++ spec/OUTLINE.md
@@ -0,0 +1,4 @@
+Basic Flask API server that will:
+
+* Generate UUIDs for email tickets
+* Read Gmail inbox for an email ticket with a UUID, and parse that email, returning the JSON to the client
